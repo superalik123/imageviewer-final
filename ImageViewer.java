f4453cee1b776caea2c946a356d19eeaf6de141e
superalik123 <44001727+superalik123@users.noreply.github.com>
2019-05-14 18:28:52 -0400

    channels(re-did)
     Forgot to commit in class

--- ImageViewer.java
+++ ImageViewer.java
@@ -255,7 +255,11 @@ public class ImageViewer
         filterList.add(new GrayScaleFilter("Grayscale"));
         filterList.add(new EdgeFilter("Edge Detection"));
         filterList.add(new FishEyeFilter("Fish Eye"));
-       
+        filterList.add(new RedFilter("Red Channel"));
+        filterList.add(new GreenFilter("Green Channel"));
+        filterList.add(new BlueFilter("Blue Channel"));
+
+        
         return filterList;
     }
     
